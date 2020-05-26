d5210ccde5d4f6e3cb9764591cc66d0b6d769a97
qiaobingbing@kewenhealth.com <qiaobingbing@kewenhealth.com>
2020-05-26 18:10:22 +0800

    v0.0.2

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,20 @@
-'use strict'
-const path = require('path')
-
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
-
+const path = require('path');
 module.exports = {
   pages: {
     index: {
       entry: 'test/main.js',
       template: 'public/index.html',
       filename: 'index.html'
-    },
+    }
   },
-  publicPath: '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
   lintOnSave: false,
-  productionSourceMap: true,
-
   configureWebpack: {
     output: {
-      // 输出重构  打包编译后的 文件名称  【模块名称.版本号】
-      filename: `js/[name].js`,
-      chunkFilename: `js/[name].js`
-    },
-    resolve: {
-      alias: {
-        '@': resolve('packages')
-      }
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bingogo.min.js',
+      library: 'bingogo',
+      libraryTarget: 'umd',
+      umdNamedDefine: true
     }
   },
   chainWebpack: config => {
